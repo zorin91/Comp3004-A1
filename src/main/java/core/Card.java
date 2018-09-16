49bca8eaaada4a4d035566918ce127245f620185
Alex <alexsent@rogers.com>
2018-09-16 13:25:48 -0400

    Added the Card class
    All test cases of CardTest passed!

---
+++ src/main/java/core/Card.java
@@ -0,0 +1,32 @@
+package core;
+
+public class Card {
+
+	private String suit,rank;
+	
+	public Card(String suit,String rank)
+	{
+		this.suit = suit;
+		this.rank = rank;
+	}
+	
+	public String getSuit()
+	{
+		return this.suit;
+	}
+	public int getValue()
+	{
+		if(this.rank == "A")
+		{
+			return 11;
+		}
+		else if(this.rank == "J" ||this.rank == "Q" ||this.rank == "K")
+		{
+			return 10;
+		}
+		else
+		{
+			return Integer.parseInt(this.rank);
+		}
+	}
+}
