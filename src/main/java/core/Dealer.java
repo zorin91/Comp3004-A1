e3833833cfd39b6365d1ccc47449e703f2cd5e2d
Alex <alexsent@rogers.com>
2018-09-16 23:01:00 -0400

    Dealer class added
    All test cases passed!

---
+++ src/main/java/core/Dealer.java
@@ -0,0 +1,66 @@
+package core;
+
+import java.util.ArrayList;
+
+public class Dealer {
+	private ArrayList<Card> hand;
+	private int handSize;
+	private boolean hasAce;
+	private boolean hasSmallAce;
+
+	public Dealer()
+	{
+		hand = new ArrayList<Card>();
+		this.handSize = 0;
+		this.hasAce = false;
+		this.hasSmallAce = false;
+	}
+	
+	public String drawCard(Card card)
+	{
+		if(card.getValue() == 11)
+		{
+			hasAce = true;
+		}
+		hand.add(card);
+		handSize++;
+		return card.toString();
+	}
+	
+	public int getHandSize()
+	{
+		return this.handSize;
+	}
+	
+	public int getHandValue()
+	{
+		int result = 0;
+		for (Card temp:hand)
+		{
+			result+= temp.getValue();
+		}
+		if(hasAce && result > 21)
+		{
+			result -= 10;
+			hasSmallAce = true;
+		}
+		return result;
+	}
+	
+	public boolean dealerDecision()
+	{
+		if(this.getHandValue() <= 16)
+		{
+			return true;
+		}
+		else if(this.getHandValue() == 17 && !this.hasSmallAce)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+	
+}
