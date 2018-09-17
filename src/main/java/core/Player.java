8402c2bb39e45e35faeae9177342ec8f067150fb
Alex <alexsent@rogers.com>
2018-09-16 22:34:04 -0400

    Added Player class
    All test cases succeeded!

---
+++ src/main/java/core/Player.java
@@ -0,0 +1,47 @@
+package core;
+
+import java.util.ArrayList;
+
+public class Player {
+	private ArrayList<Card> hand;
+	private int handSize;
+	private boolean hasAce;
+
+	public Player()
+	{
+		hand = new ArrayList<Card>();
+		this.handSize = 0;
+		this.hasAce = false;
+	}
+	
+	public String drawCard(Card card)
+	{
+		if(card.getValue() == 11)
+		{
+			hasAce = true;
+		}
+		hand.add(card);
+		handSize++;
+		return card.toString();
+	}
+	
+	public int getHandSize()
+	{
+		return this.handSize;
+	}
+	
+	public int getHandValue()
+	{
+		int result = 0;
+		for (Card temp:hand)
+		{
+			result+= temp.getValue();
+		}
+		if(hasAce && result > 21)
+		{
+			result -= 10;
+		}
+		return result;
+	}
+	
+}
